f0e0c687f9e7cde4552143754cb5686038e724d8
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-07-20 17:27:14 +0530

    created the client ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import PostCreate from "./PostCreate"
+import PostList from "./PostList"
 
 function App() {
   return (
     <div className="App">
-hi
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 }
---
+++ client/src/PostList.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    try {
+      const res = await axios.get("http://localhost:4002/posts");
+      setPosts(res.data);
+      console.log("the posts recieved " , res.data);
+    } catch (error) {
+      console.log("error in postlist " , error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+      <div
+        style={{ width: "30%", marginBottom: "20px" }}
+        key={post.id}
+      >
+        <div >
+          <h3>{post.title}</h3>
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {renderedPosts}
+    </div>
+  );
+};
+
+export default PostList;
