50d6005ff8ca800e94c59d18d54a067a69ff68be
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2024-07-22 16:56:34 +0530

    created docker and yaml file for all left over sevices and also linked the clusterip services together and checked through postman if they were working fine

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,21 +9,22 @@ const events = [];
 
 app.post("/events", (req, res) => {
   const event = req.body;
-
+  console.log("reached event bus service" , event);
   events.push(event);
 
   axios.post("http://posts-srv:4000/events", event).catch((err) => {
     console.log(err.message);
   });
-  // axios.post("http://localhost:4001/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  console.log("sent message to all other serivce through event bus");
   res.send({ status: "OK" });
 });
 
